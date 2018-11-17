ebf2a452c6c285c8c55341ea0b64a9779474e211
unknown <kshevitskiy@gmail.com>
2018-11-17 19:22:09 +0100

    Webpack configuration
    Setup hashing and serve assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,95 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
-
-// .env constants
-const ASSET_PATH = process.env.ASSET_PATH || '/';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
   context: path.resolve(__dirname, 'src'),
+
   entry: {
-    polyfill: 'babel-polyfill',
-    app: ['./app.js'],
+    main: './index.js',
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: ASSET_PATH,
+    filename: '[name].[hash:8].js',
   },
+
+  devServer: {
+    port: 3000,   
+    open: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!sass-loader',
-        })
+        use:  [
+          'style-loader', 
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'sass-loader'
+        ]        
       },
+
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env']
-            ]
-          }
         }
+      },   
+
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true
+            }
+          }
+        ]
       },
+
+      {
+        test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }      
     ]
   },
+
   devtool: 'inline-source-map',
-  devServer: {
-    port: 3000,   //Tell dev-server which port to run
-    open: true,   // to open the local server in browser
-    contentBase: path.resolve(__dirname, 'dist'), //serve from 'dist' folder
-  },
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new ExtractTextPlugin('css/styles.css'),
+    new CleanWebpackPlugin('dist', {}),
+    new CopyWebpackPlugin([
+      { 
+        from: path.resolve(__dirname, 'src/assets'), 
+        to: 'assets' 
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.[contenthash:8].css',
+    }),        
     new HtmlWebpackPlugin({
+      inject: true,
+      hash: false,
       title: 'GitHub issue tracker',
-      template: 'index.ejs'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
-    })    
-  ]
+      template: './index.html',
+    })
+  ] 
 };
 
 module.exports = config;
