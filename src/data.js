7fb3b910cab6900c769b9c24385c681f7657a6c7
unknown <kshevitskiy@gmail.com>
2018-11-18 14:07:26 +0100

    Navigation component
    Added navigation component and icons.
    Added data.js, all issues data are stored in this file

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const issues = [
+  { title: 'Issue 1', solved: false },
+  { title: 'Issue 2', solved: false },
+  { title: 'Issue 3', solved: false },
+  { title: 'Issue 4', solved: false },
+  { title: 'Issue 5', solved: false },
+  { title: 'Issue 6', solved: false },
+  { title: 'Issue 7', solved: false },
+  { title: 'Issue 8', solved: false },
+  { title: 'Issue 9', solved: false },
+  { title: 'Issue 10', solved: false },
+  { title: 'Issue 11', solved: false },
+  { title: 'Issue 12', solved: false }
+]
+
+module.exports = issues;
